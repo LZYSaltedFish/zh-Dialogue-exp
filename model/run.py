7dd5655fa856d08999436bee570d5dfeebb077c2
LZYSaltedFish <3050669253@qq.com>
2023-03-06 14:21:16 +0800

    更新

--- model/run.py
+++ model/run.py
@@ -5,10 +5,12 @@ import math
 import colossalai
 from colossalai.logging import get_dist_logger
 from colossalai.core import global_context as gpc
+from colossalai.utils import save_checkpoint
 
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from torch.utils.data import Dataset, DataLoader, RandomSampler
+from torch.utils.tensorboard import SummaryWriter
 
 
 hf_model_path = 'IDEA-CCNL/Wenzhong-GPT2-110M'
@@ -19,12 +21,17 @@ config_path = '/apsarapangu/disk3/xianyu-anaconda/zh-Dialogue-exp/model/config.p
 train_data_path = '/apsarapangu/disk3/xianyu-anaconda/zh-Dialogue-exp/dataset/plain_chitchat/pretrain_train.tsv'
 valid_data_path = '/apsarapangu/disk3/xianyu-anaconda/zh-Dialogue-exp/dataset/plain_chitchat/pretrain_valid.tsv'
 
+ckpt_dir = '/apsarapangu/disk3/xianyu-anaconda/ckpt/Wenzhong-110M/'
+
+huggingface_cache_path = '/apsarapangu/disk3/xianyu-anaconda/huggingface_cache'
+
 params = {
     'max_seq_len': 512,
     'lr': 7e-6,
     'batch_size': 1,
     'lr_scheduler_patience': 3,
-    'lr_scheduler_decay': 0.5
+    'lr_scheduler_decay': 0.5,
+    "save_ckpt_steps": 2000
 }
 
 class DialogueDataset(Dataset):
@@ -46,7 +53,7 @@ class DialogueDataset(Dataset):
         return data_rows
 
     def load_tokenizer(self, pad_token='<pad>', bos_token='<bos>', truncation_side='left'):
-        self.tokenizer = GPT2Tokenizer.from_pretrained(hf_model_path)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(hf_model_path, cache_dir=huggingface_cache_path)
         self.tokenizer.add_tokens(pad_token)
         self.tokenizer.add_tokens(bos_token)
         self.tokenizer.pad_token = pad_token
@@ -75,7 +82,7 @@ class DialogueDataset(Dataset):
             encoded_input = self.tokenizer(history,
                                            padding='max_length',
                                            truncation=True,
-                                           max_length=self.max_text_length)
+                                           max_length=self.max_seq_length)
             encoded_input.update({'label_ids': encoded_input['input_ids']})
             episodes.append(encoded_input)
         return episodes
@@ -89,13 +96,11 @@ def compute_token_loss(forward_outputs, label_ids, criterion):
     logits = forward_outputs['logits'][:, :-1]
     logits_view = logits.reshape(-1, logits.size(-1))
     loss = criterion(logits_view, label_ids.reshape(-1))
-    prob = torch.softmax(logits, dim=-1)
-    loss = loss.view(prob[:, :-1].shape[:-1]).sum(dim=1)
+    loss = loss.sum()
     return loss
 
 def compute_loss(forward_outputs, label_ids, criterion, pad_token_id):
     loss = compute_token_loss(forward_outputs, label_ids, criterion)
-    loss = loss.sum()
 
     notnull = label_ids.ne(pad_token_id)
     target_tokens = notnull.long().sum()
@@ -109,7 +114,7 @@ def compute_metrics(forward_outputs, label_ids, criterion, pad_token_id):
     logits_view = logits.reshape(-1, logits.size(-1))
     loss = criterion(logits_view, label_ids.reshape(-1))
     prob = torch.softmax(logits, dim=-1)
-    loss = loss.view(prob[:, :-1].shape[:-1]).sum(dim=1)
+    loss = loss.view(prob.shape[:-1]).sum(dim=1)
 
     notnull = label_ids.ne(pad_token_id)
     target_tokens = notnull.long().sum(dim=1)
@@ -125,15 +130,40 @@ def compute_metrics(forward_outputs, label_ids, criterion, pad_token_id):
     total_acc = sum([c/t for c in correct for t in target_tokens])
     total_em = sum([correct[i]==target_tokens[i] for i in range(len(target_tokens))])
     total_samples = len(target_tokens)
-    return total_loss, total_ppl, total_acc, total_em, total_samples 
+    return total_loss, total_ppl, total_acc, total_em, total_samples
 
+def log_tensorboard(writer,
+                    learning_rate,
+                    global_step,
+                    current_loss=None,
+                    eval_scores=None,
+                    is_training=True):
+    if is_training:
+        writer.add_scalar(tag='losses/loss',
+                          scalar_value=current_loss,
+                          global_step=global_step)
+        writer.add_scalar(tag='learning_rate/lr',
+                          scalar_value=learning_rate,
+                          global_step=global_step)
+    else:
+        for metric_name, score in eval_scores:
+            writer.add_scalar(tag=f'eval/{metric_name}',
+                              scalar_value=score,
+                              global_step=global_step)
+
+
+_global_step = 0
+best_valid_score = float('inf')
+
+tensorboard = SummaryWriter(
+    log_dir=os.path.join(ckpt_dir, 'log'))
 
 if __name__ == '__main__':
     # 1.initialize distributed environment
     colossalai.launch_from_torch(config=config_path)
 
     # 2.Create training components
-    model = GPT2LMHeadModel.from_pretrained(hf_model_path)
+    model = GPT2LMHeadModel.from_pretrained(hf_model_path, cache_dir=huggingface_cache_path)
 
     train_dataset = DialogueDataset(data_file=train_data_path, max_seq_length=params['max_seq_len'])
     valid_dataset = DialogueDataset(data_file=valid_data_path, max_seq_length=params['max_seq_len'])
@@ -191,48 +221,80 @@ if __name__ == '__main__':
             # update parameters
             engine.step()
 
-        # update learning rate
-        lr_scheduler.step()
-
-        # execute a testing iteration
-        engine.eval()
-        total_loss = 0
-        total_ppl = 0
-        total_acc = 0
-        total_em = 0
-        total_samples = 0
-        for _step, batch in enumerate(valid_dataloader):
-            try:
-                batch = {
-                    key: val.cuda() if isinstance(val, torch.Tensor) else val
-                    for key, val in batch.items()
-                }
-            except RuntimeError:
-                batch = {key: val for key, val in batch.items()}
+            _global_step += 1
+            log_tensorboard(writer=tensorboard,
+                            learning_rate=optimizer.param_groups[0]['lr'],
+                            current_loss=train_loss,
+                            global_step=_global_step)
 
 
-            # run prediction without back-propagation
-            with torch.no_grad():
-                label = batch.pop('label_ids')
-                output = engine(**batch)
-                loss, ppl, acc, em, samples = compute_metrics(output, label, engine.criterion, valid_dataset.tokenizer.pad_token_id)
-                # valid_loss = engine.criterion(output, label)
 
-            # compute the metrics
-            total_loss += loss
-            total_ppl += ppl
-            total_acc += acc
-            total_em += em
-            total_samples += samples
+            if _global_step % params['save_ckpt_steps'] == 0:
+                # Evaluation
+                logger.info(
+                    f'========== Evaluation at global step {_global_step} =========='
+                )
+                # execute a testing iteration
+                engine.eval()
+                total_loss = 0
+                total_ppl = 0
+                total_acc = 0
+                total_em = 0
+                total_samples = 0
+                for _valid_step, batch in enumerate(valid_dataloader):
+                    try:
+                        batch = {
+                            key: val.cuda() if isinstance(val, torch.Tensor) else val
+                            for key, val in batch.items()
+                        }
+                    except RuntimeError:
+                        batch = {key: val for key, val in batch.items()}
+
+
+                    # run prediction without back-propagation
+                    with torch.no_grad():
+                        label = batch.pop('label_ids')
+                        output = engine(**batch)
+                        loss, ppl, acc, em, samples = compute_metrics(output, label, engine.criterion, valid_dataset.tokenizer.pad_token_id)
+                        # valid_loss = engine.criterion(output, label)
+
+                    # compute the metrics
+                    total_loss += loss
+                    total_ppl += ppl
+                    total_acc += acc
+                    total_em += em
+                    total_samples += samples
+                
+                # cross entropy loss
+                total_loss /= total_samples
+                # perplexity
+                total_ppl /= total_samples
+                # token-wise accuracy
+                total_acc /= total_samples
+                # utterance-wise exact match
+                total_em /= total_samples
+
+                logger.info(
+                    f"Epoch {epoch} - train loss: {train_loss:.5}, test loss: {total_loss:.5}, ppl: {total_ppl:.5}, acc: {total_acc:.5}, lr: {optimizer.param_groups[0]['lr']:.5g}", ranks=[0])
+                
+                
+                # Save model
+                if total_loss < best_valid_score:
+                    saving_path = os.path.join(ckpt_dir, 'colossal_model.pt')
+                    logger.info(
+                        f'Saving best model to {saving_path}...'
+                    )
+                    save_checkpoint(saving_path, epoch, model)
+                    best_valid_score = total_loss
+                
+                metrics = [('loss', total_loss), ('ppl', total_ppl), ('acc', total_acc), ('em', total_em)]
+                log_tensorboard(writer=tensorboard,
+                                learning_rate=optimizer.param_groups[0]['lr'],
+                                eval_scores=metrics,
+                                global_step=_global_step,
+                                is_training=False)
+                
+                # update learning rate
+                lr_scheduler.step(total_loss)
+
         
-        # cross entropy loss
-        total_loss /= total_samples
-        # perplexity
-        total_ppl /= total_samples
-        # token-wise accuracy
-        total_acc /= total_samples
-        # utterance-wise exact match
-        total_em /= total_samples
-
-        logger.info(
-            f"Epoch {epoch} - train loss: {train_loss:.5}, test loss: {total_loss:.5}, ppl: {total_ppl:.5}, acc: {total_acc:.5}, lr: {lr_scheduler.get_last_lr()[0]:.5g}", ranks=[0])
