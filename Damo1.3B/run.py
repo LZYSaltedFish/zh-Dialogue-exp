47fe53853d9efcc110c42860c3bd7c0a5be1a330
LZYSaltedFish <3050669253@qq.com>
2023-03-09 22:36:15 +0800

    初始化modelscope代码

---
+++ Damo1.3B/run.py
@@ -0,0 +1,63 @@
+# finetune_dureader.py
+from torch.utils.tensorboard import SummaryWriter
+from modelscope.msdatasets import MsDataset
+from modelscope.trainers import build_trainer
+from modelscope.metainfo import Trainers
+
+
+train_data_path = '/mnt/workspace/workgroup/lizhenyu/blender_data/datasets/finetune_train.tsv'
+valid_data_path = '/mnt/workspace/workgroup/lizhenyu/blender_data/datasets/finetune_valid.tsv'
+
+dataset_dict = MsDataset.load('DuReader_robust-QG')
+
+train_dataset = dataset_dict['train'].remap_columns({'text1': 'src_txt', 'text2': 'tgt_txt'}) \
+    .map(lambda example: {'src_txt': example['src_txt'] + '\n'})
+eval_dataset = dataset_dict['validation'].remap_columns({'text1': 'src_txt', 'text2': 'tgt_txt'}) \
+    .map(lambda example: {'src_txt': example['src_txt'] + '\n'})
+
+# input_kwargs = {'delimiter': '\t'}
+# train_dataset = MsDataset.load(train_data_path, **input_kwargs)
+# valid_dataset = MsDataset.load(valid_data_path, **input_kwargs)
+
+max_epochs = 5
+
+tmp_dir = './gpt3_dureader'
+
+num_warmup_steps = 200
+
+def noam_lambda(current_step: int):
+    current_step += 1
+    return min(current_step**(-0.5),
+               current_step * num_warmup_steps**(-1.5))
+
+def cfg_modify_fn(cfg):
+    cfg.train.lr_scheduler = {
+        'type': 'LambdaLR',
+        'lr_lambda': noam_lambda,
+        'options': {
+            'by_epoch': False
+        }
+    }
+    cfg.train.optimizer = {'type': 'AdamW', 'lr': 1e-4}
+    cfg.train.dataloader = {
+        'batch_size_per_gpu': 4,
+        'workers_per_gpu': 1
+    }
+    cfg.train.hooks.append({
+        'type': 'MegatronHook'
+    })
+    cfg.preprocessor.sequence_length = 512
+    cfg.model.checkpoint_model_parallel_size = 1
+    return cfg
+
+kwargs = dict(
+    model='damo/nlp_gpt3_text-generation_1.3B',
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    max_epochs=max_epochs,
+    work_dir=tmp_dir,
+    cfg_modify_fn=cfg_modify_fn)
+
+trainer = build_trainer(
+    name=Trainers.gpt3_trainer, default_args=kwargs)
+trainer.train()
---
+++ Damo1.3B/run.sh
@@ -0,0 +1,2 @@
+# N 为模型并行度
+torchrun --nproc_per_node 1 run.py
